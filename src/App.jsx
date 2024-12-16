9195dd76e04ddafe624ad1d67c81fea11022215c
rileyeh <rhatchdev@gmail.com>
2024-12-16 12:53:41 -0700

    Revert "added banner comp"
    This reverts commit 2cb7c1acba3b7f8308ccb6ba2311bdb33e9c8c1a.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import './App.css'
-
 import {
   Route,
-  RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-} from 'react-router-dom'
-
-import Banner from './Banner.jsx'
-import Root from './Root.jsx'
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./Root.jsx";
+import "./App.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Root />}>
-      <Banner />
+    <Route path="/" element={<Root />}>
     </Route>
   )
-)
+);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
